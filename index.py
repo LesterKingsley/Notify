1e012db2572f5782e4fd9b616bcb1f5e0c0e77f6
LesterKingsley <vincelesterdecastro@gmail.com>
2022-04-08 15:42:32 +0800

    with authentication

--- index.py
+++ index.py
@@ -3,10 +3,14 @@ import datetime
 import itertools
 import json
 import os
+import pymysql
 
-#Create a Object
 
+#Global Variables
 notes=[];
+logged=False;
+_user="";
+_pass="";
 
 #Class of Word Object
 class Notes:
@@ -23,7 +27,7 @@ class Notes:
     def obj_dict(obj):
         return obj.__dict__;
     def show(self):
-        date=todays_date = datetime.datetime.now()
+        date= datetime.datetime.now()
         print("\nNOTE DETAILS","\ntitle:", self.title, "\ncontent:", self.content , "\ndate added:",date);  #pag comma automatic space na
 
 
@@ -41,33 +45,135 @@ def confirm(func,*args):
 
     
 def Main():
-
-    print("\nWelcome to Notify\n");
-    res=input("Press 'A' to add notes, 'D' to delete notes, 'B' to open notes,'E' for export,'I' for import, 'X' to exit: ");
-    if(res=="a"):
-        Add();
-    elif(res=="d"):
-        Delete()
-    elif(res=="b"):
-        browse()
-    elif(res=="e"):
-        export()
-    elif(res=="i"):
-        confirm(importer,"this will import and clear current file, continue? ",False);
-        importer()
-    elif res=="x":
-        exit();   
+    if(logged==True):
+        print("\nWelcome to Notify\n");
+        res=input("Press 'A' to add notes, 'D' to delete notes, 'B' to open notes,'E' for export,'I' for import, 'X' to exit: ");
+        if(res=="a"):
+            Add();
+        elif(res=="d"):
+            Delete()
+        elif(res=="b"):
+            browse()
+        elif(res=="e"):
+            export()
+        elif(res=="i"):
+            confirm(importer,"this will import and clear current file, continue? ",False);
+            importer()
+        elif res=="x":
+            save();
+            exit();   
+        else:
+            print("wrong input");
+            Main();
     else:
-        print("wrong input");
-        Main();
+        res=input("Press 'S' to sign in, 'L' to log in: ");
+        if(res=="s"):
+            sign();
+        elif(res=="l"):
+            login();
+        else:
+            print("wrong input");
+            Main();
+        
 
+def fetchNotes():
+    print("loading notes...");
+    conn = pymysql.connect(host="localhost",user="root",passwd="",database="notify" );
+    if conn.open:     
+        cursor=conn.cursor();
+        global _user;
+        global _pass;
+        username=_user;
+        password=_pass;
+        sql = "SELECT data FROM users WHERE username='{0}' and password='{1}'".format(username,password);
+        cursor.execute(sql)
+        result = cursor.fetchone()[0];
+        if result!="[]":
+            print(result);
+            # json.dumps([ob.__dict__ for ob in notes]);
+            imported=json.loads(result);
+            for x in imported:
+                newNote=Notes(x['title'],x['content']);
+                notes.append(newNote);
+            print("account syncing succesfully executed \n");
+            conn.commit();
+            conn.close();
+            Main();
+        else:
+            conn.commit();
+            conn.close();
+
+
+# Sign In
+def sign():
+    print("Signing In!");
+    conn = pymysql.connect(host="localhost",user="root",passwd="",database="notify" );
+    if conn.open:
+        cursor = conn.cursor();
+        username=input("Input your username: ");
+        password=input("Input your password: ");
+        
+        # queries for inserting values
+        username_exists = "SELECT * FROM users WHERE username='{0}'".format(username);
+        cursor.execute(username_exists);
+        result = cursor.fetchall();
+     
+        if(len(result)==0):
+            sign_up= "INSERT INTO users(USERNAME,PASSWORD,DATA) VALUES('{0}', '{1}', '[]');".format(username,password);
+            cursor.execute(sign_up);
+            print("signed up succesfully");
+            conn.commit();
+            conn.close();
+            login();
+        else:
+            conn.commit();
+            conn.close();
+            print("username already exists..pls try again...");
+            sign();
+
+        
+
+def login():
+    print("Logging In");
+    conn = pymysql.connect(host="localhost",user="root",passwd="",database="notify" );
+    if conn.open:
+        cursor = conn.cursor();
+        username=input("Input your username: ");
+        password=input("Input your password: ");
+        #check if that exists;
+        sql = "SELECT * FROM users WHERE username='{0}' and password='{1}'".format(username,password);
+        
+        cursor.execute(sql)
+        result = cursor.fetchall();
+        if(len(result)==0):
+            print("account not detected. create a new account");
+            conn.commit();
+            conn.close();
+            Main();
+        else:
+            global _user;
+            global _pass;
+            global logged;
+            _user=username;
+            _pass=password;
+            logged=True;        
+            print("account detected");
+            conn.commit();
+            conn.close();
+            fetchNotes();
+            
+            
+            Main();
+        
+    
+#Log In
 def Add():
     print("Add a New Note");
     title=input("Add a title:");
     content=input("Add a content:");
     newNote=Notes(title,content);
     confirm(Add,"save this note?",False);
-    notes.append(newNote);
+    notes.append(newNote); # There is no need in global statement if there is no assignment.
     # print(notes);
     Main();
 
@@ -140,6 +246,16 @@ def importer():
         importer();
     Main();
 
-
+def save():
+    print("saving...");
+    exported=json.dumps([ob.__dict__ for ob in notes]);
+    conn = pymysql.connect(host="localhost",user="root",passwd="",database="notify" );
+    if conn.open:
+        global _user; 
+        cursor=conn.cursor();
+        updateSql = "UPDATE  users SET data= '{0}'  WHERE username = '{1}' ;".format(exported,_user);
+        cursor.execute(updateSql);
+    conn.commit();
+    conn.close();
+    print("saved");
 Main()
-
